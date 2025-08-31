aff94661e88c670599f7865c9cf7da97ccc7e3ae
screws101 <142944513+screws101@users.noreply.github.com>
2025-08-30 22:01:16 -0400

    change quotes

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@
 
     function updateTitle() {
       const count = items.length;
-      titleEl.textContent = 'My Items (${count})';
+      titleEl.textContent = `My Items (${count})`;
     }
 
     function render() {
